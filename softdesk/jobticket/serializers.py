47544d95d4ed042a561809684d68063b8de608cc
Greg M <marainigregory@pm.me>
2023-03-26 11:55:48 +0200

    first commit

---
+++ softdesk/jobticket/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Project, Issue, Comment, Contributor
+
+
+class ProjectListSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'type', 'title']
+
+
+class ProjectDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'type', 'title', 'description']
+
+
+class IssueDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = ['id', 'title', 'desc', 'tag', 'priority', 'status', 'author_user', 'assigned_to']
