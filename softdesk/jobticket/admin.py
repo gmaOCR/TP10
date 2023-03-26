1d4ce549ddf555f47a3eb5f03a8f8df6ab3c069c
Greg M <marainigregory@pm.me>
2023-03-26 11:54:52 +0200

    creation de l'interface admin

--- softdesk/jobticket/admin.py
+++ softdesk/jobticket/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Project, Contributors
+from .models import Project, Contributor, Issue, Comment
 admin.site.register(Project)
-admin.site.register(Contributors)
+admin.site.register(Contributor)
+admin.site.register(Issue)
+admin.site.register(Comment)
 
 # Register your models here.
