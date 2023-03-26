2d95ce2894e7f8f3c91fc52c840f46c9d92cc36d
Greg M <marainigregory@pm.me>
2023-03-26 11:55:35 +0200

    minor change (min car)

--- softdesk/jobticket/models.py
+++ softdesk/jobticket/models.py
@@ -9,17 +9,17 @@ class Project(models.Model):
         ('IOS', 'iOS'),
         ('A', 'Android'),
     )
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000, validators=[MinLengthValidator(20)])
+    title = models.CharField(max_length=100, validators=[MinLengthValidator(5)])
+    description = models.CharField(max_length=1000, validators=[MinLengthValidator(10)])
     type = models.CharField(
         max_length=10, choices=CHOICES, verbose_name='type'
     )
     author_user = models.ForeignKey(
-        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='project_author'
     )
 
 
-class Contributors(models.Model):
+class Contributor(models.Model):
     CHOICES_PERM = (
         ('C', 'Create'),
         ('R', 'Read'),
@@ -37,11 +37,45 @@ class Contributors(models.Model):
     permission = models.CharField(max_length=15, choices=CHOICES_PERM)
 
 
-class Issues(models.Model):
-    title = models.CharField(max_length=200)
+class Issue(models.Model):
+    TAG_CHOICES = (
+        ('Bug', 'Bug'),
+        ('Improv.', 'Amélioration'),
+        ('Task', 'Tâches')
+    )
+
+    PRIORITY_CHOICES = (
+        ('Low', 'Faible'),
+        ('Medium', 'Moyen'),
+        ('High', 'Elevée')
+    )
+
+    STATUS_CHOICES = (
+        ('To do', 'A faire'),
+        ('In prog', 'En cours'),
+        ('Done', 'Terminé')
+    )
+
+    title = models.CharField(max_length=200, validators=[MinLengthValidator(5)])
     desc = models.CharField(max_length=2000, validators=[MinLengthValidator(20)])
-    tag = models.CharField(max_length=200)
-    status
-    author_user
-    assigned_to
-    created_time
+    tag = models.CharField(max_length=20, choices=TAG_CHOICES)
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    author_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='issue_author'
+    )
+    assigned_to = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='issue_assigned'
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    desc = models.CharField(max_length=2000, validators=[MinLengthValidator(5)])
+    author_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comment_author'
+    )
+    issue = models.ForeignKey(
+        to=Issue, on_delete=models.CASCADE, related_name='comment_issue'
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
