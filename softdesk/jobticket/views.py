11b21c4bd83527f9d6d322957a4ac22691fa6b7e
Greg M <marainigregory@pm.me>
2023-03-26 11:56:41 +0200

    ProjectViewSet creation

--- softdesk/jobticket/views.py
+++ softdesk/jobticket/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Project, Issue, Comment, Contributor
+from .serializers import ProjectListSerializer, ProjectDetailSerializer
+
+
+class ProjectViewSet(ModelViewSet):
+    serializer_class = ProjectListSerializer
+    detail_serializer_class = ProjectDetailSerializer
+
+    def get_queryset(self):
+        return Project.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return self.detail_serializer_class
+        return super().get_serializer_class()
