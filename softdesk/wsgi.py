645d7fefe54728a33ec80bc97efa4ed01e92df47
Greg M <marainigregory@pm.me>
2023-03-29 13:31:55 +0200

    path project change

--- softdesk/wsgi.py
+++ softdesk/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for softdeskasdsa project.
+WSGI config for softdesk project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'softdeskasdsa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'softdesk.settings')
 
 application = get_wsgi_application()
