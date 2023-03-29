51663aedd9b50209eaf287e0b5cedbbe77402d79
Greg M <marainigregory@pm.me>
2023-03-29 13:29:14 +0200

    DJANGO_SETTINGS_MODULE modif

--- softdesk/asgi.py
+++ softdesk/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for softdeskasdsa project.
+ASGI config for softdesk project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'softdeskasdsa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'softdesk.settings')
 
 application = get_asgi_application()
